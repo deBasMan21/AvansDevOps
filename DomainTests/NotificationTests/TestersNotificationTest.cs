3d9a1db25a076e6faad333cfb73cb8a2a8c618ff
Bas <bbuijsen@gmail.com>
2023-03-29 19:42:18 +0200

    Us-08 tests

--- DomainTests/NotificationTests/TestersNotificationTest.cs
+++ DomainTests/NotificationTests/TestersNotificationTest.cs
@@ -1,41 +1,54 @@
 ﻿using AvansDevOps;
 using AvansDevOps.SprintAbstraction;
 using AvansDevOps.UserAbstraction;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 
 namespace DomainTests.NotificationTests
 {
     public class TestersNotificationTest
     {
         [Fact]
-        public void TestersShouldReceiveNotificationWhenTicketGoesToReadyForTesting()
+        public void SingleTesterShouldReceiveNotificationWhenTicketGoesToReadyForTesting()
         {
+            string messageObj = "";
+            Type typeObj = null;
+
             // Arrange
-            Sprint sprint = new ReleaseSprint(
-                Name: "",
-                StartDate: DateTime.Now,
-                EndDate: DateTime.Now,
-                leadDeveloper: new LeadDeveloper(Name: "John Doe"),
-                scrumMaster: new ScrumMaster(Name: "Kapitein Haak"),
-                developers: new List<User>()
+            var mock = new Mock<ReleaseSprint>(
+                "",
+                DateTime.Now,
+                DateTime.Now,
+                new LeadDeveloper(Name: "John Doe"),
+                new ScrumMaster(Name: "Kapitein Haak"),
+                new List<User>()
             );
+            mock.CallBase = true;
+            mock.Setup(c => 
+                c.Notify(
+                    It.IsAny<string>(), 
+                    It.IsAny<Type>()
+                )
+            ).Callback((string message, Type type) => { 
+                messageObj = message; 
+                typeObj = type; 
+            }).Returns(1);
+
+            Sprint sprint = mock.Object;
 
             BacklogItem item = new BacklogItem(DefinitionOfDone: "done", Description: "hi");
             item.AssignDeveloper(new Developer(Name: "John Doe"));
             sprint.sprintBacklog.Add(item);
 
             Tester tester = new Tester("Tester");
-            Tester tester2 = new Tester("Tester2");
             sprint.AddDeveloper(tester);
-            sprint.AddDeveloper(tester2);
-            tester2.AddNotificationPreference(AvansDevOps.Enums.NotificationType.SLACK);
             tester.AddNotificationPreference(AvansDevOps.Enums.NotificationType.SLACK);
-            tester.AddNotificationPreference(AvansDevOps.Enums.NotificationType.EMAIL);
 
             item.StartTask();
 
@@ -43,24 +56,121 @@ namespace DomainTests.NotificationTests
             int result = item.FinishTask();
 
             // Assert
-            Assert.Equal(3, result);
+            Assert.Equal(1, result);
+            Assert.NotNull(typeObj);
+            Assert.Equal(typeof(Tester), typeObj);
+            Assert.NotEmpty(messageObj);
         }
+
         [Fact]
-        public void Tmp()
+        public void MultipleTestersShouldReceiveNotificationWhenTicketGoesToReadyForTesting()
         {
-            StringWriter stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
+            // Arrange
+            string messageObj = "";
+            Type typeObj = null;
 
-            Console.WriteLine("Heyyy hallo");
-            Console.WriteLine("Heyyy hallo");
-            Console.WriteLine("Heyyy hallo");
+            // Arrange
+            var mock = new Mock<ReleaseSprint>(
+                "",
+                DateTime.Now,
+                DateTime.Now,
+                new LeadDeveloper(Name: "John Doe"),
+                new ScrumMaster(Name: "Kapitein Haak"),
+                new List<User>()
+            );
+            mock.CallBase = true;
+            mock.Setup(c =>
+                c.Notify(
+                    It.IsAny<string>(),
+                    It.IsAny<Type>()
+                )
+            ).Callback((string message, Type type) => {
+                messageObj = message;
+                typeObj = type;
+            }).Returns(4);
 
-            List<string> output = stringWriter
-                .ToString()
-                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-                .ToList();
+            Sprint sprint = mock.Object;
 
-            Assert.Equal(3, output.Count);
+            BacklogItem item = new BacklogItem(DefinitionOfDone: "done", Description: "hi");
+            item.AssignDeveloper(new Developer(Name: "John Doe"));
+            sprint.sprintBacklog.Add(item);
+
+            Tester tester = new Tester("Tester");
+            sprint.AddDeveloper(tester);
+            tester.AddNotificationPreference(AvansDevOps.Enums.NotificationType.SLACK);
+            tester.AddNotificationPreference(AvansDevOps.Enums.NotificationType.EMAIL);
+
+            Tester tester2 = new Tester("Tester2");
+            sprint.AddDeveloper(tester2);
+            tester2.AddNotificationPreference(AvansDevOps.Enums.NotificationType.SLACK);
+
+            Tester tester3 = new Tester("Tester3");
+            sprint.AddDeveloper(tester3);
+            tester3.AddNotificationPreference(AvansDevOps.Enums.NotificationType.EMAIL);
+
+            item.StartTask();
+
+            // Act
+            int result = item.FinishTask();
+
+            // Assert
+            Assert.Equal(4, result);
+            Assert.NotNull(typeObj);
+            Assert.Equal(typeof(Tester), typeObj);
+            Assert.NotEmpty(messageObj);
+        }
+
+        [Fact]
+        public void SingleTesterShouldNotReceiveNotificationAfterRemovingFromTicket()
+        {
+
+            // Arrange
+            string messageObj = "";
+            Type typeObj = null;
+
+            // Arrange
+            var mock = new Mock<ReleaseSprint>(
+                "",
+                DateTime.Now,
+                DateTime.Now,
+                new LeadDeveloper(Name: "John Doe"),
+                new ScrumMaster(Name: "Kapitein Haak"),
+                new List<User>()
+            );
+            mock.CallBase = true;
+            mock.Setup(c =>
+                c.Notify(
+                    It.IsAny<string>(),
+                    It.IsAny<Type>()
+                )
+            ).Callback((string message, Type type) => {
+                messageObj = message;
+                typeObj = type;
+            }).Returns(0);
+
+            Sprint sprint = mock.Object;
+
+            BacklogItem item = new BacklogItem(DefinitionOfDone: "done", Description: "hi");
+            item.AssignDeveloper(new Developer(Name: "John Doe"));
+            sprint.sprintBacklog.Add(item);
+
+            Tester tester = new Tester("Tester");
+            sprint.AddDeveloper(tester);
+            tester.AddNotificationPreference(AvansDevOps.Enums.NotificationType.SLACK);
+            tester.AddNotificationPreference(AvansDevOps.Enums.NotificationType.EMAIL);
+
+            sprint.RemoveDeveloper(tester);
+
+            item.StartTask();
+
+            // Act
+            int result = item.FinishTask();
+
+            // Assert
+            Assert.Equal(0, result);
+            Assert.NotNull(typeObj);
+            Assert.Equal(typeof(Tester), typeObj);
+            Assert.NotEmpty(messageObj);
         }
     }
 }
